19e066324fe49f17e6d597d8b5b62277da52a6b8
Sandeep Parikh <parikhs@google.com>
2018-12-11 13:44:28 -0800

    updates to dataset parsing and loading

--- dataset/movielens.py
+++ dataset/movielens.py
@@ -1,28 +1,32 @@
 """
 
-usage: python movielens.py [movies.dat] [ratings.dat]
+usage: python movielens.py [movies] [ratings] [links]
 
 """
+
+import sys
+import re
+import csv
+import os
+import tmdbsimple as tmdb
 
 from pymongo import MongoClient
 from pymongo import ASCENDING, DESCENDING
 from datetime import datetime
-import sys
-import re
+from time import sleep
 
-def import_movies(db, fmovies):
-    regex = re.compile("(?P<movieid>[0-9]+)::(?P<title>.+?) \((?P<year>\d{4})\)::(?P<genres>.+?)\n")
 
+def import_movies(db, mfile):
     count = 0
     movies = []
-    for line in fmovies:
-        match = regex.search(line)
-        groups = match.groupdict()
+    mcsv = csv.DictReader(mfile)
+
+    for row in mcsv:
         movie = {
-            'movieid': int(groups['movieid']),
-            'title': groups['title'],
-            'year': int(groups['year']),
-            'genres': groups['genres'].split('|')
+            'movieid': int(row['movieId']),
+            'title': row['title'].split(' (')[0],
+            'year': row['title'].split(' (')[-1][:-1],
+            'genres': row['genres'].split('|')
         }
         movies.append(movie)
         count += 1
@@ -31,37 +35,37 @@ def import_movies(db, fmovies):
             # print count, "movies inserted"
             db.command('insert', 'movies', documents=movies, ordered=False)
             movies = []
+    
+    if count > 0:
+        db.command('insert', 'movies', documents=movies, ordered=False)
 
 
-
-def import_ratings(db, fratings):
-    regex = re.compile("(?P<userid>[0-9]+)::(?P<movieid>[0-9]+)::(?P<rating>.*?)::(?P<ts>.*?)\n")
-
+def import_ratings(db, rfile):
     count = 0
     ratings, movies, users = [], [], []
-    for line in fratings:
-        match = regex.search(line)
-        groups = match.groupdict()
+    rcsv = csv.DictReader(rfile)
+
+    for row in rcsv:
         rating = {
-            'movieid': int(groups['movieid']),
-            'userid': int(groups['userid']),
-            'rating': float(groups['rating']),
-            'ts': datetime.fromtimestamp(float(groups['ts']))
+            'movieid': int(row['movieId']),
+            'userid': int(row['userId']),
+            'rating': float(row['rating']),
+            'ts': datetime.fromtimestamp(float(row['timestamp']))
         }
         ratings.append(rating)
 
         movie_update = {
-            'q': { 'movieid': int(groups['movieid']) },
+            'q': { 'movieid': int(row['movieId']) },
             'u': { '$inc': {
                     'ratings' : 1,
-                    'total_rating': float(groups['rating'])
+                    'total_rating': float(row['rating'])
                 }
             }
         }
         movies.append(movie_update)
 
         user_update = {
-            'q': { 'userid' : int(groups['userid']) },
+            'q': { 'userid' : int(row['userId']) },
             'u': { '$inc': { 'ratings': 1 } },
             'upsert': True
         }
@@ -75,6 +79,37 @@ def import_ratings(db, fratings):
             db.command('update', 'movies', updates=movies, ordered=False)
             db.command('update', 'users', updates=users, ordered=False)
             ratings, movies, users = [], [], []
+
+    if count > 0:
+        db.command('insert', 'ratings', documents=ratings, ordered=False)
+        db.command('update', 'movies', updates=movies, ordered=False)
+        db.command('update', 'users', updates=users, ordered=False)
+
+
+def import_links(db, lfile):
+    count = 0
+    movies = []
+    lcsv = csv.DictReader(lfile)
+
+    for row in lcsv:
+        try:
+            movies.append({
+                'q': {'movieid': int(row['movieId'])},
+                'u': { '$set': {
+                        'imdb': row['imdbId'], 
+                        'tmdb': row['tmdbId']
+                }}
+            })
+            count += 1
+        except:
+            continue
+
+        if (count % 1000) == 0:
+            db.command('update', 'movies', updates=movies, ordered=False)
+            movies = []
+
+    if count > 0:
+        db.command('update', 'movies', updates=movies, ordered=False)
 
 
 def create_genres(db):
@@ -94,6 +129,36 @@ def create_genres(db):
     db.command('insert', 'genres', documents=genres, ordered=False)
 
 
+def update_avg_ratings(db):
+    movies = db.movies.find()
+    for m in movies:
+        try:
+            db.movies.update_one({'_id': m['_id']}, {'$set': {'avg_rating': float(m['total_rating'])/m['ratings']}})
+        except:
+            continue
+    
+
+def get_poster_links(db):
+    tmdb.API_KEY='[YOUR API KEY HERE]'
+    conf = tmdb.Configuration()
+    imgurl = conf.info()['images']['base_url'] + 'w154' + '{path}'
+
+    allmovies = db.movies.find()
+
+    for i in xrange(0, allmovies.count(), 40):
+        print i
+        for j in xrange(i, i+40):
+            try:
+                movie = tmdb.Movies(int(allmovies[j]['tmdb'])).info()
+                db.movies.update_one(
+                    {'_id': allmovies[j]['_id']},
+                    {'$set': {'poster': imgurl.format(path=movie['poster_path'])}}
+                )
+            except:
+                continue
+        sleep(10)
+
+
 def ensure_indexes(db):
     db.movies.ensure_index("movieid")
     db.movies.ensure_index("ratings")
@@ -104,15 +169,28 @@ def ensure_indexes(db):
 
 
 def main():
-    db = MongoClient()['movielens']
+    host=os.environ.get('MONGODB_HOST', 'localhost')
+    port=os.environ.get('MONGODB_PORT', 27017)
+    database=os.environ.get('MONGODB_DB', 'movieweb')
 
-    fmovies = open(sys.argv[1])
-    fratings = open(sys.argv[2])
+    db = MongoClient(host, port)[database]
 
-    import_movies(db, fmovies)
-    import_ratings(db, fratings)
+    with open(sys.argv[1]) as mfile:
+        import_movies(db, mfile)
+
+    with open(sys.argv[2]) as rfile:
+        import_ratings(db, rfile)
+
+    with open(sys.argv[3]) as lfile:
+        import_links(db, lfile)
+
     create_genres(db)
-    ensure_indexes(db)
+
+    update_avg_ratings(db)
+
+    get_poster_links(db)
+
+    # ensure_indexes(db)
 
 
 if __name__ == '__main__':
